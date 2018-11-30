c2d1eba4f8df2aa4b930cd1c038a35ce3d66124d
weichi hung <lucas861223@gmail.com>
2018-11-29 17:34:18 -0800

    test 3

--- ftfy_app.py
+++ ftfy_app.py
@@ -5,60 +5,6 @@ import json
 from urllib.parse import urlencode
 from ftfy.fixes import fix_encoding_and_explain
 
-EXAMPLES = [
-    "He's Justinâ\x9d¤",
-    'Le Schtroumpf Docteur conseille g√¢teaux et baies schtroumpfantes pour un '
-    'r√©gime √©quilibr√©.',
-    'âœ” No problems',
-    'РґРѕСЂРѕРіРµ Р\x98Р·-РїРѕРґ #С„СѓС‚Р±РѕР»',
-    '\x84Handwerk bringt dich überall hin\x93: Von der YOU bis nach Monaco',
-    'Hi guys í ½í¸\x8d',
-    'hihi RT username: â\x98ºí ½í¸\x98',
-    'Beta Haber: HÄ±rsÄ±zÄ± BÃ¼yÃ¼ Korkuttu',
-    'Р С—РЎР‚Р С‘РЎРЏРЎвЂљР Р…Р С•РЎРѓРЎвЂљР С‘. РІСњВ¤',
-    'Kayanya laptopku error deh, soalnya tiap mau ngetik deket-deket kamu font yg '
-    'keluar selalu Times New Ã¢â‚¬Å“ RomanceÃ¢â‚¬Â\x9d.',
-    'Iggy Pop (nÃƒÂ© Jim Osterberg)',
-    "Direzione Pd, ok â\x80\x9csenza modifiche\x94 all'Italicum.",
-    "selamat berpuasa sob (Ã\xa0Â¸â€¡'ÃŒâ‚¬Ã¢Å’Â£'ÃŒÂ\x81)Ã\xa0Â¸â€¡",
-    'The Mona Lisa doesnÃƒÂ¢Ã¢â€šÂ¬Ã¢â€žÂ¢t have eyebrows.',
-    '#╨┐╤Ç╨░╨▓╨╕╨╗╤î╨╜╨╛╨╡╨┐╨╕╤é╨░╨╜╨╕╨╡',
-    "LiĂ¨ge Avenue de l'HĂ´pital",
-    'It was namedÂ â€žscarsÂ´ stonesâ€ś after the rock-climbers who got hurt '
-    'while climbing on it.',
-    'vedere Ă®nceĹŁoĹźatÄ\x83',
-    'NapĂ\xadĹˇte nĂˇm !',
-    'It was namedÂ\xa0â\x80\x9escarsÂ´ stonesâ\x80\x9c after the rock-climbers '
-    'who got hurt while climbing on it.',
-    'Arsenal v Wolfsburg: pre-season friendly â\x80â\x80\x9c live!',
-    'Ã¢â€\x9dâ€™(Ã¢Å’Â£Ã‹â€ºÃ¢Å’Â£)Ã¢â€\x9dÅ½',
-    'Ã¢â€�â€™(Ã¢Å’Â£Ã‹â€ºÃ¢Å’Â£)Ã¢â€�Å½',
-    'I just figured out how to tweet emojis! â\x9a½í\xa0½í¸\x80í\xa0½í¸\x81í\xa0'
-    '½í¸\x82í\xa0½í¸\x86í\xa0½í¸\x8eí\xa0½í¸\x8eí\xa0½í¸\x8eí\xa0½í¸\x8e'
-]
-
-
-INDEX = '''
-<html>
-<head>
-</head>
-<body>
-{output}
-</html>
-'''
-
-examples = ['<ul>']
-for example in EXAMPLES:
-    steps = fix_encoding_and_explain(example)[1]
-    if steps:
-        examples.append('<li><a href="?{}">{}</a></li>'.format(
-            urlencode({'s': example}), escape(example)
-        ))
-examples.append('</ul>')
-
-
-app = Sanic(__name__)
-
 
 def steps_to_python(s, steps):
     python = ['s = {}'.format(repr(s))]
@@ -89,9 +35,7 @@ async def handle_request(request):
     if s:
         s = s[0].strip()
         fixed, steps = fix_encoding_and_explain(s)
-        return response.html(INDEX.format(
-            output=escape(fixed),
-        ))
+        return escape(fixed)
     else:
         return response.html(INDEX.format(
             output='',
